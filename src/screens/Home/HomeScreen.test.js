9cd182b9cb127c4c11a38886704fa3cdc9d9231a
Chris Morris <chrismorris@Chriss-Mac-mini.local>
2022-01-09 17:21:06 +0000

    Initial setup and test verification

---
+++ src/screens/Home/HomeScreen.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react-native';
+import HomeScreen from './HomeScreen';
+
+describe('HomeScreen', () => {
+    test('HomeScreen renders Hello', () => {
+        const screen = render(<HomeScreen />);
+        const hello = screen.getByText('Hello!');
+        expect(hello).toBeDefined();
+    });
+});
