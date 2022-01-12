ac5b0d32b6ed08da826ad09b6ad24e5bd40f7222
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-01-12 09:31:44 +0000

    Commit prior to nav refactor

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,30 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
-import { changeGreeting } from './HomeScreenSlice';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 // Import Components
 import Stats from '../../components/Stats';
 import Divider from '../../components/Divider';
 import Card from '../../components/Card';
 
-const HomeScreen = ({ navigation }) => {
-    const [greeting, setGreeting] = useState('');
-    const dispatch = useDispatch();
-    const selectedGreeting = useSelector(state => state.Home.greeting);
+// Import Context
+import { AuthContext } from '../../context/authContext';
 
-    const handlePress = () => {
-        dispatch(changeGreeting(greeting))
-    }
+const HomeScreen = ({ navigation }) => {
+    const selectedGreeting = useSelector(state => state.Home.greeting);
+    const { signOut } = useContext(AuthContext);
 
     return (
         <View style={styles.container}>
-            <Stats style={styles.stats} />
-            <Divider />
-            <Card number={selectedGreeting} />
-            <TextInput style={{ height: 20, width: 200, backgroundColor: 'white' }} value={greeting} onChangeText={setGreeting} />
-            <Button title="Say Hello" onPress={handlePress} />
-            <Button title="Play!" onPress={() => navigation.navigate('Play')} />
+            <Text style={styles.greeting} >Welcome to Yozu Games!</Text>
+            <TouchableOpacity style={styles.play} onPress={() => navigation.navigate('Play')} ><Text style={styles.buttonText} >Higher/Lower</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.play} onPress={() => signOut()} ><Text style={styles.buttonText} >Sign Out</Text></TouchableOpacity>
         </View>
     )
 }
@@ -39,7 +33,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(228, 70, 67)',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    greeting: {
+        fontSize: 30,
+        marginBottom: 30,
+        textAlign: 'center'
+    },
+    play: {
+        height: 66,
+        width: 200,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        justifyContent: 'center',
         alignItems: 'center'
+    },
+    buttonText: {
+        fontSize: 20
     }
 });
 
